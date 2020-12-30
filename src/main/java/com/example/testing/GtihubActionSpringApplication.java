7afdc2f57e6403113fda2ad82a13b6f85e47209c
16byte <8byte@naver.com>
2020-12-30 13:56:26 +0900

    Initial commit

---
+++ src/main/java/com/example/testing/GtihubActionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtihubActionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtihubActionSpringApplication.class, args);
+    }
+
+}
